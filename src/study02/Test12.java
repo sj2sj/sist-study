7eada9005eae1212a2b389163ef0b307c8972bef
sj2sj <waterttug@naver.com>
2021-07-13 14:36:57 +0900

    if/switch문 예제

---
+++ src/study02/Test12.java
@@ -0,0 +1,33 @@
+package study02;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("==== Test12 Start ====");
+		
+		int num = 1;
+		
+		switch(num) {
+			case 1:
+				System.out.println("case1");
+				break;
+			case 2:
+				System.out.println("case2");
+				break;
+			case 3:
+				System.out.println("case3");
+				break;
+			case 4:
+				System.out.println("case4");
+				break;
+			default: 
+				System.out.println("그 외 나머지 ");
+				break;
+		}
+		
+		
+		System.out.println("==== Test12 Finish ====");
+	}
+
+}
