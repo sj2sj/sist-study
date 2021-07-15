7a424daae0a7deb1f83f82e1a3d922e31a156d5a
sj2sj <waterttug@naver.com>
2021-07-15 14:12:52 +0900

    반복문 끝

---
+++ src/com/iu/s3/WhileTest03.java
@@ -0,0 +1,19 @@
+package com.iu.s3;
+
+import java.util.Scanner;
+
+public class WhileTest03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("==== WhileTest3 Start!! ====");
+		
+		Scanner sc;
+		
+		
+		System.out.println("==== WhileTest3 Finish!! ====");
+
+	}
+
+}
---
+++ src/com/iu/s4/Array_01.java
@@ -0,0 +1,10 @@
+package com.iu.s4;
+
+public class Array_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
