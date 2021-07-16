df3b93cee3e50a4464ecb84c7baede36db0fb38c
sj2sj <waterttug@naver.com>
2021-07-16 12:21:59 +0900

    배열 인덱스 지정해서 삭제하는 기능 실습

--- src/com/iu/s4/Array_05.java
+++ src/com/iu/s4/Array_05.java
@@ -23,13 +23,14 @@ public class Array_05 {
 			System.out.println("1. 정보 출력 ");
 			System.out.println("2. 정보 추가 ");
 			System.out.println("3. 정보 삭제 ");
-			System.out.println("4. 종료 ");
+			System.out.println("4. 검색 삭제 ");
+			System.out.println("5. 종료 ");
 			System.out.println("======================");
 			int select = sc.nextInt();
 			
 			
 			switch(select) {
-				case 1:
+				case 1: //정보 출력 
 					if (ar.length > 0) { //값이 있으면
 						for (int i = 0; i < ar.length; i++) {
 							System.out.print(ar[i] + " ");
@@ -39,7 +40,7 @@ public class Array_05 {
 					}
 					
 					break;
-				case 2:
+				case 2: //정보 추가 
 					System.out.println("정보를 추가합니다. ");
 					System.out.println("추가할 값: ");
 					int n = sc.nextInt(); //추가할 값 입력
@@ -54,11 +55,11 @@ public class Array_05 {
 					ar = ar2;
 					
 					break;
-				case 3:
+				case 3: //정보 삭제 
 					System.out.println("정보를 삭제합니다. (마지막 값이 삭제 됨.) ");
 					
 					if (ar.length == 0) {
-						System.out.println("더이상 삭제할 값이 없습니다. ");
+						System.out.println("더 이상 삭제할 값이 없습니다. ");
 						continue;
 					}
 					
@@ -69,6 +70,33 @@ public class Array_05 {
 						ar3[i] = ar[i];
 					}
 					ar = ar3;
+					
+					break;
+				case 4: //검색 삭제 
+					if (ar.length == 0) {
+						System.out.println("더 이상 삭제할 값이 없습니다. ");
+						continue;
+					}
+					
+					System.out.println("삭제할 인덱스 번호를 입력하세요: ");
+					int d = sc.nextInt(); //삭제할 인덱스 번호.
+	
+					if (d >= ar.length || d < 0) { //삭제하려는 인덱스가 배열 길이보다 클 때 
+						System.out.println("인덱스가 존재하지 않습니다!!");
+						continue;
+					} else { //인덱스 삭제 
+						int[] ar4 = new int[ar.length-1];
+						
+						for (int i = 0, j = 0; i < ar.length; i++) {
+							if (i == d) {
+								continue;
+							} else {
+								ar4[j] = ar[i];
+								j++;
+							}
+						}
+						ar = ar4;
+					}
 					
 					break;
 				default:
