acf5ba564af091e733e45805e9b9c85fdf73712e
sj2sj <waterttug@naver.com>
2021-07-20 12:08:18 +0900

    메서드 실습

---
+++ src/com/iu/s2/ex1/Ex1Main.java
@@ -0,0 +1,15 @@
+package com.iu.s2.ex1;
+
+public class Ex1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("==== Ex1 Start ====");
+		
+		
+		
+		
+		System.out.println("==== Ex1 Finish ====");
+	}
+
+}
