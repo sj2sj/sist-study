8645000ae2856e982dad1e0a926a6fb5b4d80a7a
sj2sj <waterttug@naver.com>
2021-07-27 14:27:53 +0900

    StringTokenizer 실습

---
+++ src/com/iu/s2/token/Phone.java
@@ -0,0 +1,23 @@
+package com.iu.s2.token;
+
+public class Phone {
+
+	private String name;
+	private String price;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+}
